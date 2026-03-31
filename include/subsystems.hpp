16a7f06d35d191be8b03802e3508b4d9d2c36fd2
2403069-wq <2403069@utmir.edu.mx>
2026-03-31 13:32:32 -0600

    first commit

---
+++ include/subsystems.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+#include "pros/motor_group.hpp"
+
+extern Drive chassis;
+
+// tren motriz
+inline pros::MotorGroup left_motors({6, 7, 8});
+inline pros::MotorGroup right_motors({-1, -2, -3});
+
+// selector
+inline pros::Motor selector(10);
+
+// almacén
+inline pros::Motor almacen(-9);
+
+// succión
+inline pros::MotorGroup intake_motors({4, 5});
+inline pros::Motor intake_expul(-12);
+
+extern bool intake_expul_activado;
+
+// pistón
+inline pros::adi::DigitalOut piston('A');
